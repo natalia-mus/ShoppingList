a84e805bf7b14321449496a17200a0f2cd7fd5bb
natalia-mus <natalia.muskala1ab1@gmail.com>
2022-11-19 18:51:06 +0100

    AddProductActivityPresenter tests

---
+++ app/src/androidTest/java/com/example/shoppinglist/presenter/AddProductActivityPresenterTest.kt
@@ -0,0 +1,39 @@
+package com.example.shoppinglist.presenter
+
+import com.example.shoppinglist.ValidationResult
+import com.example.shoppinglist.constants.SavingContext
+import com.example.shoppinglist.contract.AddProductActivityContract
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class AddProductActivityPresenterTest {
+
+    private lateinit var presenter: AddProductActivityPresenter
+
+    @Mock
+    private lateinit var mockView: AddProductActivityContract.AddProductActivityView
+
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        presenter = AddProductActivityPresenter(mockView)
+    }
+
+
+    @Test
+    fun addValidProduct() {
+        val result = presenter.saveData(SavingContext.CREATE, null, "name", "1", "1")
+        assertTrue(result == ValidationResult.VALID)
+    }
+
+    @Test
+    fun addProductWithEmptyName() {
+        val result = presenter.saveData(SavingContext.CREATE, null, "", "1", "1")
+        assertTrue(result == ValidationResult.EMPTY_NAME)
+    }
+
+}
