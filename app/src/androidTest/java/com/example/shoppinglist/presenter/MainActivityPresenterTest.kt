68d3346aebb1d4c5a599a6117e5d5924010aed70
natalia-mus <natalia.muskala1ab1@gmail.com>
2022-11-29 18:48:52 +0100

    MainActivityPresenter tests

--- app/src/androidTest/java/com/example/shoppinglist/presenter/MainActivityPresenterTest.kt
+++ app/src/androidTest/java/com/example/shoppinglist/presenter/MainActivityPresenterTest.kt
@@ -5,6 +5,7 @@ import com.example.shoppinglist.constants.SavingContext
 import com.example.shoppinglist.contract.AddProductActivityContract
 import com.example.shoppinglist.contract.MainActivityContract
 import com.example.shoppinglist.database.DBHelper
+import org.junit.After
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
@@ -12,6 +13,14 @@ import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 
 class MainActivityPresenterTest {
+
+    companion object {
+        private const val PRODUCT_1_NAME = "testProduct1Name"
+        private const val PRODUCT_2_NAME = "testProduct2Name"
+        private const val PRODUCT_1_QUANTITY = "testProduct1Quantity"
+        private const val PRODUCT_2_QUANTITY = "testProduct2Quantity"
+        private const val PRODUCT_PRIORITY = "17"
+    }
 
     private lateinit var mainActivityPresenter: MainActivityPresenter
 
@@ -24,7 +33,6 @@ class MainActivityPresenterTest {
     private lateinit var addProductActivityView: AddProductActivityContract.AddProductActivityView
 
 
-
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
@@ -32,9 +40,8 @@ class MainActivityPresenterTest {
         mainActivityPresenter = MainActivityPresenter(mainActivityView)
 
         addProductActivityPresenter = AddProductActivityPresenter(addProductActivityView)
-        addProductActivityPresenter.saveData(SavingContext.CREATE, null, "tomatoes", "2", "1")
-        addProductActivityPresenter.saveData(SavingContext.CREATE, null, "orange juice", "1 liter", "3")
-        addProductActivityPresenter.saveData(SavingContext.CREATE, null, "soap", "2 bars", "1")
+        addProductActivityPresenter.saveData(SavingContext.CREATE, null, PRODUCT_1_NAME, PRODUCT_1_QUANTITY, PRODUCT_PRIORITY)
+        addProductActivityPresenter.saveData(SavingContext.CREATE, null, PRODUCT_2_NAME, PRODUCT_2_QUANTITY, PRODUCT_PRIORITY)
     }
 
     @Test
@@ -46,7 +53,7 @@ class MainActivityPresenterTest {
         if (products != null) {
             var id: Int? = null
             for (product in products) {
-                if (product.name == "tomatoes" && product.quantity == "2" && product.priority == 1) {
+                if (product.name == PRODUCT_1_NAME && product.quantity == PRODUCT_1_QUANTITY && product.priority == PRODUCT_PRIORITY.toInt()) {
                     id = product.id
                 }
             }
@@ -68,6 +75,22 @@ class MainActivityPresenterTest {
         }
 
         assertTrue(condition)
+    }
+
+    @After
+    fun finish() {
+        mainActivityPresenter.fetchData()
+        val products = mainActivityPresenter.returnData()
+
+        if (products != null) {
+            for (product in products) {
+                if ((product.name == PRODUCT_1_NAME && product.quantity == PRODUCT_1_QUANTITY && product.priority == PRODUCT_PRIORITY.toInt()) ||
+                    (product.name == PRODUCT_2_NAME && product.quantity == PRODUCT_2_QUANTITY && product.priority == PRODUCT_PRIORITY.toInt())
+                ) {
+                    mainActivityPresenter.deleteItem(product.id)
+                }
+            }
+        }
     }
 
 }
