e3ac8a45ba9f09ba2260b13fe313aa4e0871912f
natalia-mus <natalia.muskala1ab1@gmail.com>
2024-01-06 16:09:09 +0100

    Selecting background color

--- app/src/main/java/com/example/shoppinglist/view/CreateThemeActivity.kt
+++ app/src/main/java/com/example/shoppinglist/view/CreateThemeActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import com.example.shoppinglist.ImageUtils
 import com.example.shoppinglist.R
 import com.example.shoppinglist.ValidationResult
 import com.example.shoppinglist.contract.CreateThemeActivityContract
 import com.example.shoppinglist.presenter.CreateThemeActivityPresenter
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import yuku.ambilwarna.AmbilWarnaDialog
 
 class CreateThemeActivity : AppCompatActivity(), CreateThemeActivityContract.CreateThemeActivityView {
 
@@ -27,11 +29,20 @@ class CreateThemeActivity : AppCompatActivity(), CreateThemeActivityContract.Cre
     private lateinit var productListLandscapeBackground: ImageView
     private lateinit var addProductPortraitBackground: ImageView
     private lateinit var addProductLandscapeBackground: ImageView
+    private lateinit var productListPortraitBackgroundBorder: LinearLayout
+    private lateinit var productListLandscapeBackgroundBorder: LinearLayout
+    private lateinit var addProductPortraitBackgroundBorder: LinearLayout
+    private lateinit var addProductLandscapeBackgroundBorder: LinearLayout
 
     private var productListPortraitBackgroundSource: ByteArray? = null
     private var productListLandscapeBackgroundSource: ByteArray? = null
     private var addProductPortraitBackgroundSource: ByteArray? = null
     private var addProductLandscapeBackgroundSource: ByteArray? = null
+
+    private var productListPortraitBackgroundColor: Int? = null
+    private var productListLandscapeBackgroundColor: Int? = null
+    private var addProductPortraitBackgroundColor: Int? = null
+    private var addProductLandscapeBackgroundColor: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +58,10 @@ class CreateThemeActivity : AppCompatActivity(), CreateThemeActivityContract.Cre
         productListLandscapeBackground = findViewById(R.id.create_theme_product_list_landscape_background)
         addProductPortraitBackground = findViewById(R.id.create_theme_add_product_portrait_background)
         addProductLandscapeBackground = findViewById(R.id.create_theme_add_product_landscape_background)
+        productListPortraitBackgroundBorder = findViewById(R.id.create_theme_product_list_portrait_background_border)
+        productListLandscapeBackgroundBorder = findViewById(R.id.create_theme_product_list_landscape_background_border)
+        addProductPortraitBackgroundBorder = findViewById(R.id.create_theme_add_product_portrait_background_border)
+        addProductLandscapeBackgroundBorder = findViewById(R.id.create_theme_add_product_landscape_background_border)
 
         saveButton.setOnClickListener {
             saveTheme()
@@ -76,6 +91,35 @@ class CreateThemeActivity : AppCompatActivity(), CreateThemeActivityContract.Cre
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         setBackgroundSource(requestCode, resultCode, data)
+    }
+
+    private fun getInitialColor(backgroundType: BackgroundType): Int {
+        val result: Int? = when (backgroundType) {
+            BackgroundType.PRODUCT_LIST_PORTRAIT_BACKGROUND -> {
+                productListPortraitBackgroundColor
+            }
+            BackgroundType.PRODUCT_LIST_LANDSCAPE_BACKGROUND -> {
+                productListLandscapeBackgroundColor
+            }
+            BackgroundType.ADD_PRODUCT_PORTRAIT_BACKGROUND -> {
+                addProductPortraitBackgroundColor
+            }
+            BackgroundType.ADD_PRODUCT_LANDSCAPE_BACKGROUND -> {
+                addProductLandscapeBackgroundColor
+            }
+        }
+
+        return result ?: ResourcesCompat.getColor(resources, R.color.sea_blue_dark, null)
+    }
+
+    private fun openColorPicker(backgroundType: BackgroundType) {
+        AmbilWarnaDialog(this, getInitialColor(backgroundType), object : AmbilWarnaDialog.OnAmbilWarnaListener {
+            override fun onCancel(dialog: AmbilWarnaDialog?) {}
+
+            override fun onOk(dialog: AmbilWarnaDialog?, color: Int) {
+                setBackgroundColor(backgroundType, color)
+            }
+        }).show()
     }
 
     private fun openGallery(backgroundType: BackgroundType) {
@@ -110,18 +154,37 @@ class CreateThemeActivity : AppCompatActivity(), CreateThemeActivityContract.Cre
         val backgroundTypePanel = BottomSheetDialog(this)
         val backgroundTypePanelView = LayoutInflater.from(this).inflate(R.layout.panel_background_type, findViewById(R.id.panel_background_type_container))
 
-        backgroundTypePanelView.findViewById<LinearLayout>(R.id.panel_background_type_image).setOnClickListener{
+        backgroundTypePanelView.findViewById<LinearLayout>(R.id.panel_background_type_image).setOnClickListener {
             openGallery(backgroundType)
             backgroundTypePanel.dismiss()
         }
 
-        backgroundTypePanelView.findViewById<LinearLayout>(R.id.panel_background_type_color).setOnClickListener{
-            //openColorPicker()
+        backgroundTypePanelView.findViewById<LinearLayout>(R.id.panel_background_type_color).setOnClickListener {
+            openColorPicker(backgroundType)
             backgroundTypePanel.dismiss()
         }
 
         backgroundTypePanel.setContentView(backgroundTypePanelView)
         backgroundTypePanel.show()
+    }
+
+    private fun setBackgroundColor(backgroundType: BackgroundType, color: Int?) {
+        when (backgroundType) {
+            BackgroundType.PRODUCT_LIST_PORTRAIT_BACKGROUND -> {
+                productListPortraitBackgroundColor = color
+            }
+            BackgroundType.PRODUCT_LIST_LANDSCAPE_BACKGROUND -> {
+                productListLandscapeBackgroundColor = color
+            }
+            BackgroundType.ADD_PRODUCT_PORTRAIT_BACKGROUND -> {
+                addProductPortraitBackgroundColor = color
+            }
+            BackgroundType.ADD_PRODUCT_LANDSCAPE_BACKGROUND -> {
+                addProductLandscapeBackgroundColor = color
+            }
+        }
+
+        if (color != null) setSelectedColor(backgroundType, color)
     }
 
     private fun setBackgroundSource(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -148,9 +211,39 @@ class CreateThemeActivity : AppCompatActivity(), CreateThemeActivityContract.Cre
                     }
                 }
             }
+
+            setBackgroundColor(BackgroundType.ADD_PRODUCT_LANDSCAPE_BACKGROUND, null)
         } else {
             Toast.makeText(this, resources.getString(R.string.error_message), Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun setSelectedColor(backgroundType: BackgroundType, color: Int) {
+        val imageView: ImageView
+        val border: LinearLayout
+
+        when (backgroundType) {
+            BackgroundType.PRODUCT_LIST_PORTRAIT_BACKGROUND -> {
+                imageView = productListPortraitBackground
+                border = productListPortraitBackgroundBorder
+            }
+            BackgroundType.PRODUCT_LIST_LANDSCAPE_BACKGROUND -> {
+                imageView = productListLandscapeBackground
+                border = productListLandscapeBackgroundBorder
+            }
+            BackgroundType.ADD_PRODUCT_PORTRAIT_BACKGROUND -> {
+                imageView = addProductPortraitBackground
+                border = addProductPortraitBackgroundBorder
+            }
+            BackgroundType.ADD_PRODUCT_LANDSCAPE_BACKGROUND -> {
+                imageView = addProductLandscapeBackground
+                border = addProductLandscapeBackgroundBorder
+            }
+        }
+
+        imageView.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.round_background, null))
+        imageView.drawable.setTint(color)
+        border.background.setTint(ResourcesCompat.getColor(resources, R.color.transparent_black, null))
     }
 }
 
